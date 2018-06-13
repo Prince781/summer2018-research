128dcdf6235d0fc3f7ff8f2f90dceb295d4f16a1
Prince781 <pferro@u.rochester.edu>
2018-06-13 15:34:11 -0400

    Initial work on third schedule.

--- Task_mapper2.c
+++ Task_mapper2.c
@@ -17,32 +17,54 @@
 #include <math.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <assert.h>
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 #define MAX_THREADS 1024
+#define MAX_SOCKETS 8
+#define MAX_CORES   1024
+#define MAX_HYPERTHREADS    4
 
 #define SCHED_DEFAULT_PREFIX    "default:"
 
-#define SCHED_COLOCATED		SCHED_DEFAULT_PREFIX"colocated"
-
-#define SCHED_SPREAD		SCHED_DEFAULT_PREFIX"spread"
-
-struct cpu_socket {
-  int cpus[MAX_THREADS]; // thread number (on this socket) -> CPU number
-  int num_cpus;          // = num_cores * threads_per_core
-  int cpu;
+enum sched_policy {
+    SCHED_COLOCATED,
+    SCHED_COLOCATED_HYPERTHREADS,
+    SCHED_SPREAD,
+    N_DEFAULT_SCHEDULES, 
 };
 
-struct cpu_socket sockets[MAX_THREADS];
-
-int num_sockets;
+const char *sched_names[] = {
+    [SCHED_COLOCATED] = SCHED_DEFAULT_PREFIX"colocated",
+    [SCHED_COLOCATED_HYPERTHREADS] = SCHED_DEFAULT_PREFIX"colocated2",
+    [SCHED_SPREAD] = SCHED_DEFAULT_PREFIX"spread"
+};
 
 struct cpu_info {
   int core_id;
   int sock_id;
 };
 
+/* flat list of CPUs */
 struct cpu_info cpus[MAX_THREADS];
 int num_cpus;
+
+struct cpu_core {
+    int pus[MAX_HYPERTHREADS];
+    int size;
+    int occupancy;
+};
+
+struct cpu_socket {
+    struct cpu_core cores[MAX_CORES];
+    int size;
+    int occupancy;
+};
+
+/* structured list of CPUs */
+struct cpu_socket sockets[MAX_SOCKETS];
+int num_sockets;
 
 const char *schedule;
 const char *program;
@@ -59,7 +81,7 @@ int compare_tids(const void *arg1, const void *arg2) {
   return *(pid_t *)arg1 - *(pid_t *)arg2;
 }
 
-void Schedule(int tnum, pid_t tid, int sock_id);
+void Schedule(int t, pid_t tid, int sock_id, int core_id, int ht);
 
 const char *get_username(void) {
     char *uname;
@@ -170,15 +192,23 @@ void PS(void) {
 
   qsort(threads, num_threads, sizeof threads[0], &compare_tids);
 
+  /* restart packing positions */
+  for (int s = 0; s < num_sockets; ++s) {
+      for (int c = 0; c < sockets[s].size; ++c)
+          sockets[s].cores[c].occupancy = 0;
+      sockets[s].occupancy = 0;
+  }
+
   if (strncmp(schedule, SCHED_DEFAULT_PREFIX, strlen(SCHED_DEFAULT_PREFIX)) != 0) {
       for (int t = 0; t < num_threads; ++t) {
-        Schedule(t, threads[t], thread_to_socket[t]);
+        Schedule(t, threads[t], thread_to_socket[t], -1, -1);
       }
   } else { // use an internal schedule
-      if (strcmp(schedule, SCHED_COLOCATED) == 0) {
+      if (strcmp(schedule, sched_names[SCHED_COLOCATED]) == 0) {
           if (num_threads >= num_cpus) {
               for (int t = 0; t < num_threads; ++t) {
-                  Schedule(t, threads[t], floor(t / ((double)num_threads / num_sockets)));
+                  int sock_id = floor(t / ((double)num_threads / num_sockets));
+                  Schedule(t, threads[t], sock_id, -1, -1);
               }
           } else {
               /**
@@ -186,41 +216,63 @@ void PS(void) {
                */
               int t = 0;
               for (int s = 0; s < num_sockets && t < num_threads; ++s) {
-                  for (int c = 0; c < sockets[s].num_cpus && t < num_threads; ++c) {
-                      Schedule(t, threads[t], s);
+                  for (int c = 0; c < sockets[s].size && t < num_threads; ++c) {
+                      for (int h = 0; h < sockets[s].cores[c].size && t < num_threads; ++h) {
+                          Schedule(t, threads[t], s, c, h);
+                          ++t;
+                      }
+                  }
+              }
+          }
+      } else if (strcmp(schedule, sched_names[SCHED_SPREAD]) == 0) {  // spread
+          for (int t = 0; t < num_threads; ++t) {
+              Schedule(t, threads[t], t % num_sockets, -1, -1);
+          }
+      } else {  // colocated onto cores first before hyperthreads
+          int t = 0;
+          while (t < num_threads) {
+              for (int s = 0; s < num_sockets && t < num_threads; ++s) {
+                  for (int c = 0; c < sockets[s].size && t < num_threads; ++c) {
+                      Schedule(t, threads[t], s, c, -1);
                       ++t;
                   }
               }
           }
-      } else {  // spread
-          for (int t = 0; t < num_threads; ++t) {
-              Schedule(t, threads[t], t % num_sockets);
-          }
       }
-  }
-
-  for (int s = 0; s < num_sockets; ++s) {
-    /**
-     * Schedule threads to the exact same hardware contexts
-     * the next time around, by starting off at the same location
-     * again.
-     */
-    sockets[s].cpu = 0;
   }
 } // function close
 
-void Schedule(int tnum, pid_t tid, int sock_id) {
-  int cpu = sockets[sock_id].cpus[sockets[sock_id].cpu];
+void Schedule(int t, pid_t tid, int sock_id, int core_id, int ht) {
+  assert(sock_id >= 0 && sock_id < num_sockets);
+
+  if (core_id == -1)
+      core_id = sockets[sock_id].occupancy;
+
+  assert(core_id >= 0 && core_id < sockets[sock_id].size);
+
+  if (ht == -1)
+      ht = sockets[sock_id].cores[core_id].occupancy;
+
+  assert(ht >= 0 && ht < sockets[sock_id].cores[core_id].size);
+
+  int cpu = sockets[sock_id].cores[core_id].pus[ht];
   cpu_set_t set;
   CPU_ZERO(&set);
   CPU_SET(cpu, &set);
   if (sched_setaffinity(tid, sizeof set, &set) != -1) {
-    printf("Set #%d TID:%d to CPU:%d on socket %d\n", tnum, tid, cpu, sock_id);
-    sockets[sock_id].cpu =
-        (sockets[sock_id].cpu + 1) % sockets[sock_id].num_cpus;
+      printf("Set thread #%d (%d) to CPU %d (socket%d/core%d/hyperthread#%d)\n",
+              t, tid, cpu, sock_id, core_id, ht);
+
+      ht = (ht + 1) % sockets[sock_id].cores[core_id].size;
+      sockets[sock_id].cores[core_id].occupancy = ht;
+      if (ht == 0) {
+          // increment core_id
+          core_id = (core_id + 1) % sockets[sock_id].size;
+          sockets[sock_id].occupancy = core_id;
+      }
   } else {
-    fprintf(stderr, "Failed to set TID %d to CPU %d: %s\n", tid, cpu,
-            strerror(errno));
+      fprintf(stderr, "Failed to move thread #%d (%d) to CPU %d: %s\n",
+              t, tid, cpu, strerror(errno));
   }
 }
 
@@ -246,6 +298,7 @@ const struct cpu_info *get_cpu(int i) {
   } else
     return NULL;
 
+
   return &info;
 }
 
@@ -258,29 +311,48 @@ void init_CPUs(void) {
     num_cpus = i + 1;
   }
 
-  for (int i = 0; i < num_cpus; ++i) {
-    int sock_cpus = sockets[cpus[i].sock_id].num_cpus;
-    sockets[cpus[i].sock_id].cpus[sock_cpus] = i;
-    sockets[cpus[i].sock_id].num_cpus++;
-    num_sockets = cpus[i].sock_id > num_sockets ? cpus[i].sock_id : num_sockets;
+  if (num_cpus != nprocs) {
+      perror("fopen");
+      abort();
   }
 
-  num_sockets++;
+  struct list {
+      int items[MAX_THREADS];
+      int size;
+  };
+
+  struct list socks[MAX_SOCKETS];
+
+  // group into sockets
+  for (int i = 0; i < num_cpus; ++i) {
+      int len = socks[cpus[i].sock_id].size++;
+      socks[cpus[i].sock_id].items[len] = i;
+      num_sockets = MAX(cpus[i].sock_id+1, num_sockets);
+      assert(num_sockets < MAX_SOCKETS);
+  }
+
+  // group into cores
+  for (int s = 0; s < num_sockets; ++s) {
+      for (int i = 0; i < socks[s].size; ++i) {
+          int i2 = socks[s].items[i];   // cpu number
+          int len = sockets[s].cores[cpus[i2].core_id].size++;
+          sockets[s].cores[cpus[i2].core_id].pus[len] = i2;
+          sockets[s].size = MAX(cpus[i2].core_id +1, sockets[s].size);
+          assert(sockets[s].size < MAX_CORES);
+          assert(sockets[s].cores[cpus[i2].core_id].size < MAX_HYPERTHREADS);
+      }
+  }
 }
 
 bool init_schedule(void) {
   FILE *fp;
   
   if (strncmp(schedule, SCHED_DEFAULT_PREFIX, strlen(SCHED_DEFAULT_PREFIX)) == 0) {
-    if (strcmp(schedule, SCHED_COLOCATED) == 0
-        || strcmp(schedule, SCHED_SPREAD) == 0)
-        return true;
-    else {
-        fprintf(stderr, "Unknown internal schedule '%s'. Options are '%s' and '%s'.\n",
-                strchr(schedule, ':') + 1, strchr(SCHED_COLOCATED, ':') + 1,
-                strchr(SCHED_SPREAD, ':') + 1);
-        return false;
-    }
+    for (int i = 0; i < N_DEFAULT_SCHEDULES; ++i)
+        if (strcmp(schedule, sched_names[i]) == 0)
+            return true;
+    fprintf(stderr, "Unknown internal schedule '%s'.\n", schedule);
+    return false;
   }
 
   bool ret = true;
@@ -340,11 +412,15 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  printf("Topology: %d threads across %d sockets:\n", num_cpus, num_sockets);
+  printf("Topology: %d hardware contexts across %d sockets:\n", num_cpus, num_sockets);
   for (int i = 0; i < num_sockets; ++i) {
-    printf(" socket %d has threads:", i);
-    for (int j = 0; j < sockets[i].num_cpus; ++j)
-      printf(" %d", sockets[i].cpus[j]);
+    printf(" socket %d has contexts: ", i);
+    for (int j = 0; j < sockets[i].size; ++j) {
+        printf("[");
+        for (int k = 0; k < sockets[i].cores[j].size; ++k)
+          printf("%d%s", sockets[i].cores[j].pus[k], k == sockets[i].cores[j].size-1 ? "" : " ");
+        printf("] ");
+    }
     printf("\n");
   }
 
