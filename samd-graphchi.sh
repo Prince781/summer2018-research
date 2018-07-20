6a7d3a25e19cd8b9c1fc698b5869ec180dac71b1
Prince781 <pferro@u.rochester.edu>
2018-07-20 02:28:46 -0400

    Update samd test script.

--- samd-graphchi.sh
+++ samd-graphchi.sh
@@ -1,98 +1,107 @@
 #!/bin/bash
 
+commands=()
+
+commands+=("runtime bin/example_apps/pagerank file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10")
+commands+=("runtime bin/example_apps/communitydetection file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10 execthreads 22")
+commands+=("runtime bin/example_apps/connectedcomponents file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10")
+commands+=("msf-total-runtime bin/example_apps/minimumspanningforest file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10")
+commands+=("runtime bin/example_apps/matrix_factorization/als_edgefactors file /u/pferro/Downloads/netflix.mm niters 10")
+commands+=("runtime bin/example_apps/stronglyconnectedcomponents file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10")
+commands+=("runtime bin/example_apps/trianglecounting file /localdisk/pferro/Downloads/soc-LiveJournal1.txt niters 10 execthreads 24")
+commands+=("runtime toolkits/collaborative_filtering/sgd --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --sgd_lambda=1e-4 --sgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22")
+commands+=("runtime toolkits/collaborative_filtering/biassgd --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --biassgd_lambda=1e-4 --biassgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22")
+commands+=("runtime toolkits/collaborative_filtering/svdpp --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --biassgd_lambda=1e-4 --biassgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22")
+commands+=("runtime toolkits/collaborative_filtering/als --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --lambda=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22")
+
 # get_runtime <param>
 function get_runtime() {
-	local param=$1
-	grep "\\.$param=" graphchi_metrics.txt | sed "s/^.*\\.$param=\\([0-9.]\\+\\).*$/\\1/"
+    local param=$1
+    grep "\\.$param=" graphchi_metrics.txt | sed "s/^.*\\.$param=\\([0-9.]\\+\\).*$/\\1/"
 }
 
 # average (list)
 function avg() {
-	echo ${@:1} | tr " " "\\n" | awk '{sum+=$1}END{print (sum/NR)}'
+    echo ${@:1} | tr " " "\\n" | awk '{sum+=$1}END{print (sum/NR)}'
 }
 
 # stdev (list)
 function stdev() {
-	echo ${@:1} | tr " " "\\n" | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}' 
+    echo ${@:1} | tr " " "\\n" | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}' 
 }
 
 # cleanup $dir
 function cleanup() {
-        local cdir=$1
-	if [[ $cdir = /u/${SUDO_USER}/* ]] || [[ $cdir = /localdisk/${SUDO_USER}/* ]]; then
-		find $cdir -type f -user root -exec rm -f {} \;
-		find $cdir -type d -user root -exec rm -rf {} \;
-	else
-		echo "Refusing to destroy contents of non-user directory $cdir"
-	fi
+    local cdir=$1
+    if [[ $cdir = /u/${SUDO_USER}/* ]] || [[ $cdir = /localdisk/${SUDO_USER}/* ]]; then
+        find $cdir -type f -user root -exec rm -f {} \;
+        find $cdir -type d -user root -exec rm -rf {} \;
+    else
+        echo "Refusing to destroy contents of non-user directory $cdir"
+    fi
 }
 
-# run_test <appname> <runtime-param> <cmd>
-function run_test() {
-	local appname=$1
-        local runtime_param=$2
-	local cmd=${@:3}
-	local stats=`readlink -fm stats/samd/graphchi/${appname}-runtimes.txt`
-	local count=8
+# test_cmd <stats> <runtime_param> <actual_cmd> ...
+function test_cmd() {
+    local stats=$1
+    local runtime_param=$2
+    local actual_cmd=${@:3}
+    local runtimes=()
+    local count=1
 
-        if [ ! -e $(dirname $stats) ]; then
-            mkdir -p $(dirname $stats)
-        fi
+    for i in $(seq 1 $count); do
+        echo edgelist | ${actual_cmd[@]}
+        tm=$(get_runtime $runtime_param)
+        runtimes+=($tm)
+        echo "$tm s" >> $stats
+    done
 
-	cd graphchi-cpp
-
-        export GRAPHCHI_DIR=$(pwd)
-	trap "{ cleanup $GRAPHCHI_DIR; }" EXIT SIGINT SIGTERM
-
-        if [ $UID -eq 0 ]; then
-            chown $SUDO_USER $stats
-            chgrp $SUDO_GROUP $stats
-        fi
-	cat <(echo $appname) <(perl -e "printf '-' x ($(wc -m <<< $appname) - 1)") <(echo "") <(echo "Command: $cmd") <(echo "") > $stats
-
-        declare -A schedules
-        schedules=( ["With samd"]="sam-launch" \
-            ["Without samd"]="" )
-
-	# try with colocated.sched
-	for s in "${!schedules[@]}"; do
-		echo $s: >> $stats
-                local actual_cmd=(${schedules[$s]} $cmd)
-		local runtimes=()
-
-                for i in $(seq 1 $count); do
-                        echo edgelist | ${actual_cmd[@]}
-			tm=$(get_runtime $runtime_param)
-			runtimes+=($tm)
-			echo "$tm s" >> $stats
-		done
-
-		echo "" >> $stats
-		echo "`avg ${runtimes[@]}` s (avg) +/- `stdev ${runtimes[@]}`" >> $stats
-		echo "" >> $stats
-	done
-
-	cd ..
+    echo "" >> $stats
+    echo "`avg ${runtimes[@]}` s (avg) +/- `stdev ${runtimes[@]}`" >> $stats
+    echo "" >> $stats
 }
 
-# run_test pagerank runtime bin/example_apps/pagerank file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10
+# run_tests
+function run_tests() {
+    cd graphchi-cpp
 
-# run_test communitydetection runtime bin/example_apps/communitydetection file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10 execthreads 22
+    if (( $? )); then
+        return
+    fi
 
-# run_test connectedcomponents runtime bin/example_apps/connectedcomponents file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10
+    export GRAPHCHI_DIR=$(pwd)
+    trap "{ cleanup $GRAPHCHI_DIR; }" EXIT SIGINT SIGTERM
 
-# run_test minimumspanningforest msf-total-runtime bin/example_apps/minimumspanningforest file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10
+    if [ $UID -eq 0 ]; then
+        chown $SUDO_USER $stats
+        chgrp $SUDO_GROUP $stats
+    fi
 
-# run_test als_edgefactors runtime bin/example_apps/matrix_factorization/als_edgefactors file /u/pferro/Downloads/netflix.mm niters 10 
+    for prefix in {sam-launch,}; do
+        local pids=()
+        for cmd in "${commands[@]}"; do
+            local runtime_param=${cmd[1]}
+            local appname=`basename ${cmd[2]}`
+            local stats=`readlink -fm stats/samd/graphchi/${prefix}/${appname}-runtimes.txt`
+            local actual_cmd=($prefix ${cmd:1})
 
-# run_test stronglyconnectedcomponents runtime bin/example_apps/stronglyconnectedcomponents file /u/pferro/Downloads/soc-LiveJournal1.txt niters 10
+            if [ ! -e $(dirname $stats) ]; then
+                mkdir -p $(dirname $stats)
+            fi
 
-# run_test trianglecounting runtime bin/example_apps/trianglecounting file /localdisk/pferro/Downloads/soc-LiveJournal1.txt niters 10 execthreads 24
+            cat <(echo $appname) <(perl -e "printf '-' x ($(wc -m <<< $appname) - 1)") <(echo "") <(echo "Command: $cmd") <(echo "") > $stats
+            test_cmd $stats $runtime_param ${actual_cmd[@]} &>/dev/null &
+            pids+=($!)
+        done
+        if [ -z $prefix ]; then
+            echo "Running control test"
+        else
+            echo "Running experimental test '$prefix'"
+        fi
+        wait ${pids[@]}
+    done
 
-# run_test sgd runtime toolkits/collaborative_filtering/sgd --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --sgd_lambda=1e-4 --sgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22
+    cd ..
+}
 
-# run_test biassgd runtime toolkits/collaborative_filtering/biassgd --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --biassgd_lambda=1e-4 --biassgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22
-
-# run_test svdpp runtime toolkits/collaborative_filtering/svdpp --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --biassgd_lambda=1e-4 --biassgd_gamma=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22
-
-run_test als runtime toolkits/collaborative_filtering/als --training=/u/pferro/Downloads/netflix/netflix_train.txt --validation=/u/pferro/Downloads/netflix/netflix_test.txt --lambda=1e-4 --minval=1 --maxval=5 --max_iter=6 --execthreads=22
+run_tests
