5453b97b45bfd344ceb982e03b6d3a9a8ce97f83
Prince781 <pferro@u.rochester.edu>
2018-08-20 16:01:44 -0400

    Parse latency.

--- cloudsuite/dc-analyze.sh
+++ cloudsuite/dc-analyze.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -E "^  1.[[:digit:]]+," | sed 's/,//g' | awk 'BEGIN{print "time, rps\n"}; {sum+=$1;sum_rps+=$2; printf "%.3f, %.3f\n", sum, $2}END{printf "Total time: %.3f s\n", sum; printf "Average requests/s: %.3f\n", (sum_rps/NR)}'
+grep -E "^  1.[[:digit:]]+," | sed 's/,//g' | awk 'BEGIN{print "time, rps, latency"}; {sum_time+=$1;sum_rps+=$2;sum_lat+=$8; printf "%.3f, %.3f, %.3f\n", sum_time, $2, $8}END{printf "Total time: %.3f s\n", sum_time; printf "Average requests/s: %.3f\n", (sum_rps/NR); printf "Average latency: %.3f\n", (sum_lat/NR)}'
