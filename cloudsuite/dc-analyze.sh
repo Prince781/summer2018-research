49c2f4ae2ba53c35de35891ceac2c2b691286b54
Prince781 <pferro@u.rochester.edu>
2018-08-20 12:05:00 -0400

    look for lines that start with 1.\d+

--- cloudsuite/dc-analyze.sh
+++ cloudsuite/dc-analyze.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -E "  1.[[:digit:]]+," | sed 's/,//g' | awk 'BEGIN{print "time, rps\n"}; {sum+=$1;sum_rps+=$2; printf "%.3f, %.3f\n", sum, $2}END{printf "Total time: %.3f s\n", sum; printf "Average requests/s: %.3f\n", (sum_rps/NR)}'
+grep -E "^  1.[[:digit:]]+," | sed 's/,//g' | awk 'BEGIN{print "time, rps\n"}; {sum+=$1;sum_rps+=$2; printf "%.3f, %.3f\n", sum, $2}END{printf "Total time: %.3f s\n", sum; printf "Average requests/s: %.3f\n", (sum_rps/NR)}'
